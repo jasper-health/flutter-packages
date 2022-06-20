89cbc99f6c3a79ea8074071913fa64d8236e968c
Eshank Vaish <eshank.vaish@icanbwell.com>
2022-06-20 15:00:35 +0530

    [CARE-713]: Updated isVisible

--- packages/pointer_interceptor/lib/src/web.dart
+++ packages/pointer_interceptor/lib/src/web.dart
@@ -28,7 +28,7 @@ void _registerFactory({bool debug = false}) {
       htmlElement.style.backgroundColor = 'rgba(255, 0, 0, .5)';
     }
     return htmlElement;
-  }, isVisible: false);
+  }, isVisible: true);
 }
 
 /// The web implementation of the `PointerInterceptor` widget.
